bf197a2d188789af67eac3d6e3aaa02b1ba62570
didaclis <36189205+didaclis@users.noreply.github.com>
2018-09-18 18:09:07 +0200

    ex vec3

---
+++ Project1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "vector3.h"
+
+int main()
+{
+	vec3<double> v1(1,2,3);
+	vec3<double> v2(1, 1, 1);
+	vec3<double> v3 = v1 + v2;
+	v3.name();
+	v1.zero();
+	v1.is_zero();
+	v2.is_zero();
+	system("pause");
+	return 0;
+}
